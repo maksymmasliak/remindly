ac518ebf1c3f3d06f22585317c872b73468cad28
MAKSYM MASLIAK <maslyak.maks@gmail.com>
2026-07-15 10:14:45 +0300

    Add Task CRUD: Form Request, Policy, and controller

---
+++ src/app/Http/Controllers/TaskController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use App\Http\Requests\StoreTaskRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class TaskController extends Controller
+{
+    use AuthorizesRequests;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        $this->authorize('viewAny', Task::class);
+        $tasks = auth()->user()->tasks;
+        return view('tasks.index', compact('tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        return view('tasks.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaskRequest $request): RedirectResponse
+    {
+        $request->user()->tasks()->create($request->validated());
+        return redirect()->route('tasks.index')->with('success', 'Задачу успішно створено!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task): View
+    {
+        $this->authorize('view', $task);
+        return view('tasks.show', compact('task'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task): View
+    {
+        $this->authorize('update', $task);
+        return view('tasks.edit', compact('task'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StoreTaskRequest $request, Task $task): RedirectResponse
+    {
+        $this->authorize('update', $task);
+        $task->update($request->validated());
+
+        return redirect()->route('tasks.index')->with('success', 'Задачу успішно оновлено!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task): RedirectResponse
+    {
+        $this->authorize('delete', $task);
+        $task->delete();
+
+        return redirect()->route('tasks.index')->with('success', 'Задачу успішно видалено!');
+    }
+}
